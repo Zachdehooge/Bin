75bc64269e6d7a59453a551c9dc358b2466eed75
zachdehooge <zacharydehooge@gmail.com>
2026-07-29 16:38:53 -0400

    Initial

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// TODO: File Information (file name, type, architecture, size)
+func fileInformation() {}
+
+// TODO: Hashes (SHA-256, etc.)
+func hashes() {}
+
+// TODO: Virus Total (take the hash and put into virus total)
+func virusTotal() {}
+
+// TODO: Strings (Runs the string command on the exe)
+func strings() {}
+
+func main() {
+	fmt.Println("Binary Information")
+	fmt.Println("------------------")
+}
