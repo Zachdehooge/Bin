7713e8c28bdf6fd21550c980f8f1eee102544575
zachdehooge <zacharydehooge@gmail.com>
2026-07-30 11:51:03 -0400

    Notes for MVP

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import "fmt"
 // TODO: File Information (file name, type, architecture, size)
 func fileInformation() {}
 
-// TODO: Hashes (SHA-256, etc.)
+// TODO: Hashes (SHA-256, etc.) - https://github.com/Zachdehooge/VTScan/blob/main/vtscan.go
 func hashes() {}
 
-// TODO: Virus Total (take the hash and put into virus total)
+// TODO: Virus Total (take the hash and put into virus total) - https://github.com/Zachdehooge/VTScan/blob/main/vtscan.go
 func virusTotal() {}
 
 // TODO: Strings (Runs the string command on the exe)
